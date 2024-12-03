17508852b92519eed5b27e9e13fdbc525e19aa87
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-03 19:37:30 +0000

    Complete tasks 1,2 and 3
    - Task 1 involved delaring objects
    - Task 2 was reading values
    - Task 3 was applying bracket notation

---
+++ main.js
@@ -0,0 +1,51 @@
+const myHouse = {
+    stories: 2,
+    parking: false,
+    bedrooms: 2,
+    garden: false,
+    energyRating: "D"
+}
+
+const myCar = {
+    color: "red",
+    wheels: 4,
+    powerSteering: true,
+    seats: 2,
+    milesPerGallon: 20
+}
+
+const myFavouriteFilm = {
+    title: "The Dark Knight",
+    releaseDate: "2008",
+    leadActor: "Christian Bale"
+}
+
+let person = {
+    name: "Ben",
+    likesChocolate: false,
+  };
+
+  console.log(person.likesChocolate)
+
+let likesChocolate = true;
+
+  if (person.likesChocolate) {
+    console.log ("Ben loves chocolate")
+  }
+  else {
+    console.log ("Ben hates chocolate")
+  }
+
+
+let desiredPleasantry = "greeting";
+
+let bensPhrases = {
+  greeting: "Well hello there!",
+  farewell: "See you in another life, brother",
+  smallTalk: "Warm today, isn't it...",
+};
+
+console.log (bensPhrases["smallTalk"]);
+
+desiredPleasantry = "farewell";
+console.log (bensPhrases[desiredPleasantry]);
